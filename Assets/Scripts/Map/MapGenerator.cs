639cc14611ac533c6187f16b72a519d5cff4001e
NeneWolf <ineslobo@hotmail.com>
2023-10-21 00:29:16 +0100

    Map Complete
    Map Details spawn fixed
    
    3 Key buildings spawn added ( x amount of villages, always 1 castle, always 1 enemy castle )

--- Assets/Scripts/Map/MapGenerator.cs
+++ Assets/Scripts/Map/MapGenerator.cs
@@ -1,10 +1,5 @@
-using System.Collections;
 using System.Collections.Generic;
-using System.Drawing;
-using Unity.VisualScripting;
-using UnityEditor.AssetImporters;
 using UnityEngine;
-using UnityEngine.UIElements;
 
 
 public class MapGenerator : MonoBehaviour
@@ -27,6 +22,8 @@ public class MapGenerator : MonoBehaviour
     [SerializeField]private float noiseScale = 0.3f;
     //[SerializeField] private float noiseFrequency = 100f;
 
+    float currentHeight;
+
     [Range(0, 1)]
     public float persistance;
     public float lacunarity;
@@ -46,9 +43,24 @@ public class MapGenerator : MonoBehaviour
     public TileTypes[] regions;
 
     public bool useFallOff;
-
+    public bool spawnDecor;
+    bool hasCleaned = true;
 
     GameObject hexagonTile;
+
+
+    //Map Decorations
+    [Header("Map Decorations")]
+
+    GameObject detail;
+    [SerializeField] GameObject[] KeyBuildings;
+    [SerializeField]int numberOfVillages;
+    public List<GameObject> playerArea;
+    public List<GameObject> enemyArea;
+
+    bool hasSpawnVillage = false;
+    bool hasSpawnCastle = false;
+    bool hasSpawnEnemyCastle = false;
 
 
     private void Awake()
@@ -63,15 +75,6 @@ public class MapGenerator : MonoBehaviour
 
     public void GenerateMap()
     {
-        if (HexParent.gameObject.transform.childCount != 0)
-        {
-            foreach (Transform child in HexParent.transform)
-            {
-                DestroyImmediate(child.gameObject);
-            }
-        }
-            
-
         GeneratePerlinNoise();
         MapDisplay display = FindAnyObjectByType<MapDisplay>();
 
@@ -97,6 +100,21 @@ public class MapGenerator : MonoBehaviour
     }
     void HexMapGenerator()
     {
+        //clean the list
+        if(playerArea.Count > 0) playerArea.Clear();
+        if(enemyArea.Count > 0) enemyArea.Clear();
+
+
+        //Create the "HexParent" Object
+        if (HexParent != null)
+        {
+            DestroyImmediate(HexParent);
+        }
+
+        HexParent = new GameObject("HexParentTiles");
+        HexParent.transform.position = Vector3.zero;
+        HexParent.transform.tag = "MapTiles";
+
         //Generate Hexagon Tiles
         for (int y = 0; y < mapHeight; y++)
         {
@@ -108,13 +126,12 @@ public class MapGenerator : MonoBehaviour
                     noiseMap[x, y] = Mathf.Clamp01(noiseMap[x, y] - fallOffMap[x, y]);
                 }
 
-                float currentHeight = noiseMap[x, y];
+                currentHeight = noiseMap[x, y];
 
                 for (int i = 0; i < regions.Length; i++)
                 {
-                    if (currentHeight <= regions[i].height && currentHeight > 0.2f) // Change this condition depending on the tile near the "lava"
+                    if (currentHeight <= regions[i].height && currentHeight > 0.10f) // Change this condition depending on the tile near the "lava"
                     {
-
                         //Create Empty Parent for HexTile
                         emptyParent = new GameObject("HexTileParent");
                         emptyParent.transform.parent = HexParent.transform;
@@ -128,53 +145,118 @@ public class MapGenerator : MonoBehaviour
     
                         hexagonTile.transform.localScale = new Vector3(5, meshHeightCurve.Evaluate(noiseMap[x, y]) * meshHeightMultiplier, 5);
                         hexagonTile.transform.parent = emptyParent.transform;
+                        AddTilesIntoSection(i);
 
-                        ////// Spawn the object on top of the hexagon
-                        if (regions[i].detailPrefabs.Length > 0)
+
+                        //Remove coment to add the decor
+                        //// Spawn the object on top of the hexagon
+                        if (spawnDecor)
                         {
-                            int randomIndex = Random.Range(0, regions[i].detailPrefabs.Length);
-                            GameObject detail = Instantiate(regions[i].detailPrefabs[randomIndex], hexagonTile.transform.position, hexagonTile.transform.transform.rotation, emptyParent.transform);
-                            detail.GetComponent<DetailMovement>().UpdatePosition();
-                        }
+                            SpawnMapDecoration(hexagonTile, i);
 
+                        }
+ 
                         break;
+                    }
+                }
+            }
+        }
+
+        //Spawn Key Buildings
+        if (spawnDecor)
+        {
+            if (numberOfVillages != 0 && hasSpawnVillage == false)
+            {
+                for (int z = 0; z < numberOfVillages; z++)
+                {
+                    int randomTile = Random.Range(0, playerArea.Count);
+                    hexagonTile = playerArea[randomTile];
+                    detail = Instantiate(KeyBuildings[0], playerArea[randomTile].gameObject.transform.position, playerArea[randomTile].transform.transform.rotation, playerArea[randomTile].gameObject.transform.parent);
+
+                    detail.GetComponent<DetailMovement>().UpdatePosition();
+                    playerArea.RemoveAt(randomTile);
+                }
+            }
+
+            if (hasSpawnCastle == false)
+            {
+                int randomTile = Random.Range(0, playerArea.Count);
+                hexagonTile = playerArea[randomTile];
+                detail = Instantiate(KeyBuildings[1], playerArea[randomTile].gameObject.transform.position, playerArea[randomTile].transform.transform.rotation, playerArea[randomTile].gameObject.transform.parent);
+
+                detail.GetComponent<DetailMovement>().UpdatePosition();
+                playerArea.Clear();
+            }
+
+            if (hasSpawnEnemyCastle == false)
+            {
+                int randomTile = Random.Range(0, enemyArea.Count);
+                hexagonTile = enemyArea[randomTile];
+                detail = Instantiate(KeyBuildings[2], enemyArea[randomTile].transform.position, enemyArea[randomTile].transform.transform.rotation, enemyArea[randomTile].gameObject.transform.parent);
+                detail.GetComponent<DetailMovement>().UpdatePosition();
+                enemyArea.Clear();
+            }
+        }
+    }
+
+    void SpawnMapDecoration(GameObject hexagonTile, int i)
+    {
+        float randomIndex = Random.Range(0f, 1f);
+
+        print(randomIndex);
+        if (regions[i].detailPrefabs.Length > 0)
+        {
+            //Spawn the decoration
+            if(randomIndex > 0.5)
+            {
+                if (regions[i].detailPrefabs.Length > 1)
+                {
+                    int random = Random.Range(0, regions[i].detailPrefabs.Length);
+
+                    detail = Instantiate(regions[i].detailPrefabs[random].detailPrefab, hexagonTile.transform.position, hexagonTile.transform.transform.rotation, emptyParent.transform);
+                    detail.GetComponent<DetailMovement>().UpdatePosition();
+
+                    if (playerArea.Contains(hexagonTile.gameObject)) { playerArea.Remove(hexagonTile.gameObject); }
+                    else if (enemyArea.Contains(hexagonTile.gameObject)) { enemyArea.Remove(hexagonTile.gameObject); }
+                }
+                else
+                {
+                    float random = Random.Range(0, 100);
+
+                    if (random < regions[i].detailPrefabs[0].spawnChange)
+                    {
+                        detail = Instantiate(regions[i].detailPrefabs[0].detailPrefab, hexagonTile.transform.position, hexagonTile.transform.transform.rotation, emptyParent.transform);
+                        detail.GetComponent<DetailMovement>().UpdatePosition();
+
+                        if (playerArea.Contains(hexagonTile.gameObject)) { playerArea.Remove(hexagonTile.gameObject);} 
+                        else if(enemyArea.Contains(hexagonTile.gameObject)) { enemyArea.Remove(hexagonTile.gameObject); }
                     }
                 }
             }
         }
     }
 
+    void AddTilesIntoSection(int i)
+    {
+        //Spawn the villages
+        if (regions[i].height <= 0.6f)
+        {
+            // add tile into the list of playerArea
+            playerArea.Add(hexagonTile.gameObject);
+        }
+        else if (regions[i].height <= 1f && regions[i].height > 0.75f)
+        {
+            // add tile into the list of enemyArea
+            enemyArea.Add(hexagonTile.gameObject);
+        }
+    }
+
+    //Return the parent that has just spawned
     public GameObject ReturnTileParent()
     {
         return hexagonTile;
     }
 
-    //private void OnDrawGizmos()
-    //{
-    //    for (int x = 0; x < mapHeight; x++)
-    //    {
-    //        for (int z = 0; z < mapWidth; z++)
-    //        {
-
-    //            Vector3 centerPosition = HexMetrics.Center(hexSize, x, z, hexOrientation) + transform.position;
-    //            for (int s = 0; s < HexMetrics.GetCorners(hexSize, hexOrientation).Length; s++)
-    //            {
-    //                Vector3 start = HexMetrics.GetCorners(hexSize, hexOrientation)[s] + centerPosition;
-    //                Vector3 end = HexMetrics.GetCorners(hexSize, hexOrientation)[(s + 1) % HexMetrics.GetCorners(hexSize, hexOrientation).Length] + centerPosition;
-    //                Gizmos.DrawLine(start, end);
-    //            }
-    //        }
-    //    }
-    //}
-
-    private void DeleteAllchildren()
-    {
-        int childCount = this.gameObject.transform.childCount;
-        for (int i = 0; i < childCount; i++)
-        {
-            DestroyImmediate(this.gameObject.transform.GetChild(0).gameObject);
-        }
-    }
 
     private void OnValidate()
     {
@@ -193,8 +275,14 @@ public class MapGenerator : MonoBehaviour
         public string name;
         public float height;
         public GameObject tilePrefab;
-        public GameObject[] detailPrefabs;
+        public TileDecor[] detailPrefabs;
+    }
 
+    [System.Serializable]
+    public struct TileDecor
+    {
+        public GameObject detailPrefab;
+        public float spawnChange;
     }
 }
 
